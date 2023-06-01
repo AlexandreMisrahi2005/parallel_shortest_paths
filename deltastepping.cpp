848372bc228cbb64b5da90c03e614f79cc82ae04
AlexandreMisrahi2005 <alex.misrahi@icloud.com>
2023-06-01 15:38:31 +0200

    cyclical bucket fix

--- deltastepping.cpp
+++ deltastepping.cpp
@@ -3,25 +3,28 @@
 
 #include "graph.h"
 
-using namespace std;
-
 // constants
 const int INF = numeric_limits<int>::max(); // virtual infinity
 int DELTA;                                  // parameter of Delta-Stepping
 int b;                                      // number of buckets
 int N;                                      // number of vertices
 
-bool isNotEmpty(vector<std::priority_queue<int> > bucket_vec){
-    for(size_t i = 0; i < bucket_vec.size(); ++i){
-        if(!bucket_vec[i].empty()){
+bool isNotEmpty(std::vector<std::priority_queue<int> > B){
+    std::cout << "here 8: length of B: " << std::endl;
+    std::cout << B.size() << std::endl;
+    for(size_t i = 0; i < B.size(); ++i){
+        if(!B[i].empty()){
             return true;
         }
     }
     return false;
 }
 
-void relax(int w, int x, vector<int> &tent, vector< std::priority_queue<int> > &B){
+void relax(int w, int x, std::vector<int> &tent, std::vector< std::priority_queue<int> > &B){
     if(x < tent[w]){
+        std::cout << "here 20: " << std::endl;
+        std::cout << (tent[w]/DELTA)%b << std::endl;
+        std::cout << (x/DELTA)%b << std::endl;
         if(tent[w] != INF){
 
             B[(tent[w]/DELTA)%b].pop();   // remove from old bucket
@@ -31,8 +34,8 @@ void relax(int w, int x, vector<int> &tent, vector< std::priority_queue<int> > &
     }
 }
 
-vector<pii> findRequests(std::priority_queue<int> bucket, bool light_edge, vector< vector<pii> > adj_list, vector<int> tent){
-    vector<pii> requests;
+std::vector<pii> findRequests(std::priority_queue<int> bucket, bool light_edge, std::vector< std::vector<pii> > adj_list, vector<int> tent){
+    std::vector<pii> requests;
 
     while (!bucket.empty()) {
         int i = bucket.top();
@@ -50,7 +53,7 @@ vector<pii> findRequests(std::priority_queue<int> bucket, bool light_edge, vecto
     return requests;
 }
 
-void relaxRequests(vector<pii> requests, vector<int> &tent, vector< std::priority_queue<int> > &B){
+void relaxRequests(std::vector<pii> requests, std::vector<int> &tent, std::vector< std::priority_queue<int> > &B){
     for(int i = 0; i < requests.size(); ++i){
         relax(requests[i].first, requests[i].second, tent, B);
     }
@@ -62,13 +65,14 @@ void relaxRequests(vector<pii> requests, vector<int> &tent, vector< std::priorit
  * tent: result array of length N, contains the shortest paths to source
  * N: number of nodes in adj_list, length of tent
 */
-vector<int> seqDeltaStepping(const Graph& graph, int source, int _DELTA, int _b) {
+std::vector<int> seqDeltaStepping(const Graph& graph, int source, int _DELTA, int _b) {
     N = graph.size();
     DELTA = _DELTA;
     b = _b;
-    vector< std::priority_queue<int> > B(b); // b buckets (vectors) stored in B, priority queues
-    vector<int> tent(N); // tentative distances
-    vector< vector<pii> > adj_list = graph.get_adj_list();
+    std::cout << "b: " << b << std::endl;
+    std::vector< std::priority_queue<int> > B(b); // b buckets (vectors) stored in B, priority queues
+    std::vector<int> tent(N); // tentative distances
+    std::vector< std::vector<pii> > adj_list = graph.get_adj_list();
 
     for(int i = 0; i < N; ++i){
         tent[i] = INF;
@@ -77,9 +81,10 @@ vector<int> seqDeltaStepping(const Graph& graph, int source, int _DELTA, int _b)
     tent[source] = 0;
     B[0].push(source);
     int i = b;
-
+    std::cout << "here 0" << std::endl;
     while(isNotEmpty(B)){
-
+        std::cout << "here 1: length of bucket vector: " << std::endl;
+        std::cout << B.size() << std::endl;
         // problem here due to modulo b?
         for(size_t j = 0; j < b; ++j){
             if( !(B[j].empty()) ){
@@ -87,19 +92,22 @@ vector<int> seqDeltaStepping(const Graph& graph, int source, int _DELTA, int _b)
                 break;
             }
         }
-
+        std::cout << "here 2" << std::endl;
         std::priority_queue<int> R;
         while( !(B[i].empty()) ){
-
-            vector<pii> Req_light = findRequests(B[i], true, adj_list, tent);
+            std::cout << "here 3" << std::endl;
+            std::vector<pii> Req_light = findRequests(B[i], true, adj_list, tent);
             R = B[i];
             B[i] = std::priority_queue<int>();
-
+            std::cout << "here 4" << std::endl;
             relaxRequests(Req_light, tent, B);
-
+            std::cout << "here 5" << std::endl;
         }
-        vector<pii> Req_heavy = findRequests(R, false, adj_list, tent);
+        std::vector<pii> Req_heavy = findRequests(R, false, adj_list, tent);
+        std::cout << "here 6" << std::endl;
         relaxRequests(Req_heavy, tent, B);
+        std::cout << "here 7" << std::endl;
     }
+    std::cout << "finish" << std::endl;
     return tent;
 }
--- main.cpp
+++ main.cpp
@@ -35,16 +35,16 @@ int main(int argc, char* argv[]) {
     0 2 3 8 6 9
     */
 
-    // Graph g(6);
+    Graph g(6);
 
-    // g.add_edge(0, 1, 2);
-    // g.add_edge(0, 2, 4);
-    // g.add_edge(1, 2, 1);
-    // g.add_edge(1, 3, 7);
-    // g.add_edge(2, 4, 3);
-    // g.add_edge(3, 5, 1);
-    // g.add_edge(4, 5, 5);
-    // g.add_edge(4, 3, 2);
+    g.add_edge(0, 1, 2);
+    g.add_edge(0, 2, 4);
+    g.add_edge(1, 2, 1);
+    g.add_edge(1, 3, 7);
+    g.add_edge(2, 4, 3);
+    g.add_edge(3, 5, 1);
+    g.add_edge(4, 5, 5);
+    g.add_edge(4, 3, 2);
 
     /*
     Test Graph
@@ -53,51 +53,51 @@ int main(int argc, char* argv[]) {
     0 6 4 3 11 10 5 7 14 18 11 10 8 10 18
     */
 
-    Graph g(15);
-    g.add_edge(0, 1, 6);
-    g.add_edge(0, 2, 4);
-    g.add_edge(0, 3, 3);
-    g.add_edge(1, 0, 6);
-    g.add_edge(1, 4, 5);
-    g.add_edge(1, 5, 9);
-    g.add_edge(2, 0, 4);
-    g.add_edge(2, 5, 6);
-    g.add_edge(2, 6, 8);
-    g.add_edge(3, 0, 3);
-    g.add_edge(3, 6, 2);
-    g.add_edge(3, 7, 4);
-    g.add_edge(4, 1, 5);
-    g.add_edge(4, 8, 3);
-    g.add_edge(4, 9, 7);
-    g.add_edge(5, 1, 9);
-    g.add_edge(5, 2, 6);
-    g.add_edge(5, 10, 5);
-    g.add_edge(6, 2, 8);
-    g.add_edge(6, 3, 2);
-    g.add_edge(6, 11, 6);
-    g.add_edge(7, 3, 4);
-    g.add_edge(7, 11, 3);
-    g.add_edge(7, 12, 1);
-    g.add_edge(8, 4, 3);
-    g.add_edge(8, 9, 6);
-    g.add_edge(8, 13, 9);
-    g.add_edge(9, 4, 7);
-    g.add_edge(9, 8, 6);
-    g.add_edge(9, 14, 2);
-    g.add_edge(10, 5, 5);
-    g.add_edge(10, 11, 4);
-    g.add_edge(10, 12, 3);
-    g.add_edge(11, 6, 6);
-    g.add_edge(11, 7, 3);
-    g.add_edge(11, 10, 4);
-    g.add_edge(12, 7, 1);
-    g.add_edge(12, 10, 3);
-    g.add_edge(12, 13, 2);
-    g.add_edge(13, 8, 9);
-    g.add_edge(13, 12, 2);
-    g.add_edge(13, 14, 8);
-    g.add_edge(14, 9, 2);
-    g.add_edge(14, 13, 8);
+    // Graph g(15);
+    // g.add_edge(0, 1, 6);
+    // g.add_edge(0, 2, 4);
+    // g.add_edge(0, 3, 3);
+    // g.add_edge(1, 0, 6);
+    // g.add_edge(1, 4, 5);
+    // g.add_edge(1, 5, 9);
+    // g.add_edge(2, 0, 4);
+    // g.add_edge(2, 5, 6);
+    // g.add_edge(2, 6, 8);
+    // g.add_edge(3, 0, 3);
+    // g.add_edge(3, 6, 2);
+    // g.add_edge(3, 7, 4);
+    // g.add_edge(4, 1, 5);
+    // g.add_edge(4, 8, 3);
+    // g.add_edge(4, 9, 7);
+    // g.add_edge(5, 1, 9);
+    // g.add_edge(5, 2, 6);
+    // g.add_edge(5, 10, 5);
+    // g.add_edge(6, 2, 8);
+    // g.add_edge(6, 3, 2);
+    // g.add_edge(6, 11, 6);
+    // g.add_edge(7, 3, 4);
+    // g.add_edge(7, 11, 3);
+    // g.add_edge(7, 12, 1);
+    // g.add_edge(8, 4, 3);
+    // g.add_edge(8, 9, 6);
+    // g.add_edge(8, 13, 9);
+    // g.add_edge(9, 4, 7);
+    // g.add_edge(9, 8, 6);
+    // g.add_edge(9, 14, 2);
+    // g.add_edge(10, 5, 5);
+    // g.add_edge(10, 11, 4);
+    // g.add_edge(10, 12, 3);
+    // g.add_edge(11, 6, 6);
+    // g.add_edge(11, 7, 3);
+    // g.add_edge(11, 10, 4);
+    // g.add_edge(12, 7, 1);
+    // g.add_edge(12, 10, 3);
+    // g.add_edge(12, 13, 2);
+    // g.add_edge(13, 8, 9);
+    // g.add_edge(13, 12, 2);
+    // g.add_edge(13, 14, 8);
+    // g.add_edge(14, 9, 2);
+    // g.add_edge(14, 13, 8);
 
 
     vector<int> dist;
