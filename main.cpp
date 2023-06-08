a9e2dd1e87912618eef7375a38f36aa29abafe1e
AlexandreMisrahi2005 <alex.misrahi@icloud.com>
2023-06-08 23:32:42 +0200

    command line + tests
    better command line arguments and adapted for testing/input graph files
    
    added a class of random graphs (grids)

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ Graph genRandomGraph(int n, int m){
 /*
 Function to parse an RMAT file into a Graph object
 */
-Graph parseRMAT(int n, std::string filename = "rmat.txt"){
+Graph parseGraph(int n, std::string filename = "graphs/g1.txt"){
     std::ifstream file(filename);
     if (!file) {
         std::cerr << "Error: Failed to open the file." << std::endl;
@@ -64,107 +64,109 @@ Graph parseRMAT(int n, std::string filename = "rmat.txt"){
     return g;
 }
 
+Graph genGridGraph(int n, double edge_proba){
+    Graph g(n);
+    double w;
+    double proba;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            int node = i * n + j;
+            
+            // left node
+            if (j > 0) {
+                int left = node - 1;
+                w = static_cast <double> (rand()) / static_cast <double> (RAND_MAX); // random double in [0,1]
+                proba = static_cast <double> (rand()) / static_cast <double> (RAND_MAX); // random double in [0,1]
+                if(proba > edge_proba){
+                    g.add_edge(node, left, w);
+                }
+            }
+
+            // right node
+            if (j < n - 1) {
+                int right = node + 1;
+                w = static_cast <double> (rand()) / static_cast <double> (RAND_MAX); // random double in [0,1]
+                proba = static_cast <double> (rand()) / static_cast <double> (RAND_MAX); // random double in [0,1]
+                if(proba > edge_proba){
+                    g.add_edge(node, right, w);
+                }
+            }
+
+            // north node
+            if (i > 0) {
+                int north = node - n;
+                w = static_cast <double> (rand()) / static_cast <double> (RAND_MAX); // random double in [0,1]
+                proba = static_cast <double> (rand()) / static_cast <double> (RAND_MAX); // random double in [0,1]
+                if(proba > edge_proba){
+                    g.add_edge(node, north, w);
+                }
+            }
+
+            // south node
+            if (i < n - 1) {
+                int south = node + n;
+                w = static_cast <double> (rand()) / static_cast <double> (RAND_MAX); // random double in [0,1]
+                proba = static_cast <double> (rand()) / static_cast <double> (RAND_MAX); // random double in [0,1]
+                if(proba > edge_proba){
+                    g.add_edge(node, south, w);
+                }
+            }
+        }
+    }
+    return g;
+}
+
 int main(int argc, char *argv[])
 {
 
     if (argc < 2)
     {
-        std::cout << "Usage: " << argv[0] << " <algorithm to use: [dijkstra, deltastepping]> "
-                  << " <optional: delta for deltastepping algorithm: [integer]> " << std::endl;
+        std::cout << "Usage: " << argv[0] 
+                  << " \n<N: number of nodes in input graph> "
+                  << " \n<path to input graph file: each line should be a triple u, v, w where u, v are node ids and w is the weight of the edge (u,v). If path is 'random', a random graph with N nodes is generated (the number of edges can be modified in main.cpp). If path is 'grid', a random grid of size n is generated with edges between each node with a probability> "
+                  << " \n<algorithm to use: [dijkstra, deltastepping]> "
+                  << " \n<optional: delta for deltastepping algorithm: [float]> " << std::endl;
         return 1;
     }
 
-    /*
-    Test graph from Abdul Bari (youtube)
-    with source node 0
-    result should be:
-    0 2 3 8 6 9
-    */
+    // parse arguments
+    int N = std::stoi(argv[1]);
+    std::string graph_file = argv[2];
+    std::string algo = argv[3];
 
-    // Graph g(6);
-
-    // g.add_edge(0, 1, 2);
-    // g.add_edge(0, 2, 4);
-    // g.add_edge(1, 2, 1);
-    // g.add_edge(1, 3, 7);
-    // g.add_edge(2, 4, 3);
-    // g.add_edge(3, 5, 1);
-    // g.add_edge(4, 5, 5);
-    // g.add_edge(4, 3, 2);
+    Graph g(N);
+    if(graph_file == "random"){
+        g = genRandomGraph(N, 100); // change number of edges in the second argument
+    }
+    else if (graph_file == "grid"){
+        g = genGridGraph(N, 0.9); // change edge probability in the second argument
+    }
+    else{
+        g = parseGraph(N, graph_file);
+    }
 
     /*
-    Test Graph
-    with source node 0
-    result should be:
-    0 6 4 3 11 10 5 7 14 18 11 10 8 10 18
+    TEST GRAPHS:
+
+        graphs/g1.txt
+        Test graph from Abdul Bari (youtube)
+        with source node 0
+        result should be:
+        0 2 3 8 6 9
+
+        graphs/g2.txt
+        Test Graph
+        with source node 0
+        result should be:
+        0 6 4 3 11 10 5 7 14 18 11 10 8 10 18
+
+        graphs/rmat_small.txt, graphs/rmat_medium.txt, graphs/rmat_large.txt
+        Test RMAT:
+        rmat_small.txt: 100 nodes, 7000 edges
+        rmat_medium.txt: 10 000 nodes, 70 000 edges
+        rmat_large.txt: 1 000 000 nodes, 80 000 000 edges
+
     */
-
-    // Graph g(15);
-    // g.add_edge(0, 1, 6);
-    // g.add_edge(0, 2, 4);
-    // g.add_edge(0, 3, 3);
-    // g.add_edge(1, 0, 6);
-    // g.add_edge(1, 4, 5);
-    // g.add_edge(1, 5, 9);
-    // g.add_edge(2, 0, 4);
-    // g.add_edge(2, 5, 6);
-    // g.add_edge(2, 6, 8);
-    // g.add_edge(3, 0, 3);
-    // g.add_edge(3, 6, 2);
-    // g.add_edge(3, 7, 4);
-    // g.add_edge(4, 1, 5);
-    // g.add_edge(4, 8, 3);
-    // g.add_edge(4, 9, 7);
-    // g.add_edge(5, 1, 9);
-    // g.add_edge(5, 2, 6);
-    // g.add_edge(5, 10, 5);
-    // g.add_edge(6, 2, 8);
-    // g.add_edge(6, 3, 2);
-    // g.add_edge(6, 11, 6);
-    // g.add_edge(7, 3, 4);
-    // g.add_edge(7, 11, 3);
-    // g.add_edge(7, 12, 1);
-    // g.add_edge(8, 4, 3);
-    // g.add_edge(8, 9, 6);
-    // g.add_edge(8, 13, 9);
-    // g.add_edge(9, 4, 7);
-    // g.add_edge(9, 8, 6);
-    // g.add_edge(9, 14, 2);
-    // g.add_edge(10, 5, 5);
-    // g.add_edge(10, 11, 4);
-    // g.add_edge(10, 12, 3);
-    // g.add_edge(11, 6, 6);
-    // g.add_edge(11, 7, 3);
-    // g.add_edge(11, 10, 4);
-    // g.add_edge(12, 7, 1);
-    // g.add_edge(12, 10, 3);
-    // g.add_edge(12, 13, 2);
-    // g.add_edge(13, 8, 9);
-    // g.add_edge(13, 12, 2);
-    // g.add_edge(13, 14, 8);
-    // g.add_edge(14, 9, 2);
-    // g.add_edge(14, 13, 8);
-
-
-    /*
-    Test random graph
-    with source node 0
-    */
-    // g = genRandomGraph(1<<20, 1<<18);
-
-
-    /*
-    Test RMAT:
-    rmat_small.txt: 100 nodes, 7000 edges
-    rmat_medium.txt: 10 000 nodes, 70 000 edges
-    rmat_large.txt: 1 000 000 nodes, 80 000 000 edges
-    */
-    // Graph g(100);
-    // g = parseRMAT(100, "rmat_small.txt");
-    // Graph g(10000);
-    // g = parseRMAT(10000, "rmat_medium.txt");
-    Graph g(1000000);
-    g = parseRMAT(1000000, "rmat_large.txt");
 
 
     std::vector<double> dist_seq;
@@ -172,15 +174,15 @@ int main(int argc, char *argv[])
     std::vector<double> dist_dij;
 
     // Find the shortest paths from vertex 0
-    std::string algo = argv[1];
     if (algo == "deltastepping")
     {
         std::vector<std::vector<Pii>> adjMat = g.get_adj_list();
+        std::cout << adjMat.size() << std::endl;
         Pii p = smallestAndLongestEdges(adjMat, g.size());
         double DELTA = p.first;
-        if (argc == 3)
+        if (argc == 5)
         {
-            DELTA = std::stod(argv[2]);
+            DELTA = std::stod(argv[4]);
         }
         int b = 1 + std::ceil(p.second / DELTA);
         b = 500000;
@@ -204,6 +206,22 @@ int main(int argc, char *argv[])
         end = std::chrono::high_resolution_clock::now();
         duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
         std::cout << "Parallel operation took " << duration << " µs." << std::endl;
+
+
+        // print the shortest paths if graph is small
+        if(N < 20){
+                std::cout << "Sequential operation" << std::endl;
+            std::cout << "Vertex\tDistance from Source" << std::endl;
+            for (int i = 0; i < g.size(); ++i) {
+                std::cout << i << "\t" << dist_seq[i] << std::endl;
+            }
+
+            std::cout << "Parallel operation" << std::endl;
+            std::cout << "Vertex\tDistance from Source" << std::endl;
+            for (int i = 0; i < g.size(); ++i) {
+                std::cout << i << "\t" << dist_par[i] << std::endl;
+            }
+        }
     } 
     else if (algo == "dijkstra") {
         dist_dij = dijkstra(g, 0);
@@ -214,21 +232,8 @@ int main(int argc, char *argv[])
         }
     }
 
-    // print the shortest paths
-    // std::cout << "Sequential operation" << std::endl;
-    // std::cout << "Vertex\tDistance from Source" << std::endl;
-    // for (int i = 0; i < g.size(); ++i) {
-    //     std::cout << i << "\t" << dist_seq[i] << std::endl;
-    // }
-
-    // std::cout << "Parallel operation" << std::endl;
-    // std::cout << "Vertex\tDistance from Source" << std::endl;
-    // for (int i = 0; i < g.size(); ++i) {
-    //     std::cout << i << "\t" << dist_par[i] << std::endl;
-    // }
-
-    std::ofstream outFile("output_sssp.txt");
-    for (const auto &e : dist_seq) outFile << e << "\n";
+    std::ofstream outFile("output_dijkstra.txt");
+    for (const auto &e : dist_dij) outFile << e << "\n";
 
     return 0;
 }
